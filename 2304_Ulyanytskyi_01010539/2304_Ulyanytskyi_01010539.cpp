0203181427e174ba6cad87fb5c045c40c217c176
Remas Orca <remas.orca@gmail.com>
2023-06-06 20:40:22 +0300

    Add project files.

---
+++ 2304_Ulyanytskyi_01010539/2304_Ulyanytskyi_01010539.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    const int start_num = 100;
+    const int end_num = 999;
+    int counter = 0;
+
+    for (int i = 0; i < end_num - start_num; i++) {
+        int units = i % 10;
+        int decade = (i / 10) % 10;
+        int hundreds = i / 100;
+
+        if (hundreds == decade || hundreds == units || decade == units) {
+            ++counter;
+        }            
+    }
+
+    return 0;
+}
