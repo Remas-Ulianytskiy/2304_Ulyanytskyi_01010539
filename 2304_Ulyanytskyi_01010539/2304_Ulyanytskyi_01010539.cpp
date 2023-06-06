437b1f7ddcc47c846c776e794e5f8473ac3bbd24
Remas Orca <remas.orca@gmail.com>
2023-06-06 21:33:22 +0300

    v2

--- 2304_Ulyanytskyi_01010539/2304_Ulyanytskyi_01010539.cpp
+++ 2304_Ulyanytskyi_01010539/2304_Ulyanytskyi_01010539.cpp
@@ -4,12 +4,14 @@ int main()
 {
     const int start_num = 100;
     const int end_num = 999;
+    const int difference = end_num - start_num;
+
     int counter = 0;
 
     for (int i = 0; i < end_num - start_num; i++) {
-        int units = i % 10;
-        int decade = (i / 10) % 10;
-        int hundreds = i / 100;
+        int units = (i + difference) % 10;
+        int decade = ((i + difference) / 10) % 10;
+        int hundreds = (i + difference) / 100;
 
         if (hundreds == decade || hundreds == units || decade == units) {
             ++counter;
